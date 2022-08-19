2d8144dcb64dd35cb237d6dbf9f2c17148085f00
Riaj Mazumder <75942383+Riyaz-mazumder@users.noreply.github.com>
2022-08-19 11:24:42 +0600

    Add files via upload

---
+++ JavaScript/For-Loop_even-odd_numbers/app.js
@@ -0,0 +1,25 @@
+// understanding the concept of modules
+
+// for (var i = 0; i <= 100; i++) {
+//   Will print out actual numbers ont the remainder only when criteria matches.
+//   if (i % 2) {
+//     console.log(i);
+//   }
+// }
+// Will print out the remainder.
+// let x = 6;
+// console.log(x % 2);
+
+// even numbers Loop
+for (var i = 0; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+// odd numbers Loop
+// for (var i = 0; i <= 100; i++) {
+//   if (i % 2) {
+//     console.log(i);
+//   }
+// }
