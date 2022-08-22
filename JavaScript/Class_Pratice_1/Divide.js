90f98fda3e57162e204a48c891a1bb834ed1c51e
Riyaz <riajmazumder711@gmail.com>
2022-08-22 14:06:52 +0600

    adding dynamic form

--- JavaScript/Class_Pratice_1/Divide.js
+++ JavaScript/Class_Pratice_1/Divide.js
@@ -1,10 +1,11 @@
-var leftOperand =77;
-var rightOperand =20>30;
+var leftOperand = 77;
+var rightOperand = 20 > 30;
 var divEm = leftOperand / rightOperand;
-var nada =0/0;
-document.write(divEm + "<p>" + nada + "</P>")
-if(!isFinite(divEm)){
-    alert("Who Dude thats a big number")
-}if(isNaN(nada)){
-    alert('You are dividing nothing by nothing')
+var nada = 0 / 0;
+document.write(divEm + '<p>' + nada + '</P>');
+if (!isFinite(divEm)) {
+  alert('Who Dude thats a big number');
+}
+if (isNaN(nada)) {
+  alert('You are dividing nothing by nothing');
 }
---
+++ JavaScript/Custom_Methods.js
@@ -0,0 +1,15 @@
+// //Here is a list of built-in number methods in JavaScript.
+
+// // Method	Description
+// isNaN()	determines whether the passed value is NaN
+// isFinite()	determines whether the passed value is a finite number
+// isInteger()	determines whether the passed value is an integer
+// isSafeInteger()	determines whether the passed value is a safe integer
+// parseFloat(string)	converts the numeric floating string to floating-point number
+// parseInt(string, [radix])	converts the numeric string to integer
+// toExponential(fractionDigits)	returns a string value for a number in exponential notation
+// toFixed(digits)	returns a string value for a number in fixed-point notation
+// toPrecision()	returns a string value for a number to a specified precision
+// toString([radix])	returns a string value in a specified radix(base)
+// valueof()	returns the numbers value
+// toLocaleString()	returns a string with a language sensitive representation of a number
